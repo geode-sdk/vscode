01c415177fe485adf25a197636c72d575139a1dd
Pauline <git@ethanlibs.co>
2024-06-16 19:48:59 +0200

    🐛 fix(buildsystem): initial silly

---
+++ eslint.config.js
@@ -0,0 +1,6 @@
+// @ts-check
+const petal = require('@flowr/eslint-config').default;
+
+module.exports = petal({
+    ignores: ['out', 'dist'],
+});
---
+++ tsup.config.ts
@@ -0,0 +1,8 @@
+import { defineConfig } from 'tsup';
+
+export default defineConfig({
+    format: 'cjs',
+    target: 'node',
+    entry: ['./src/extension.ts'],
+    external: ['vscode', 'sharp'],
+});
